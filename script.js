82c5dfc435fdfd157e207ddc0f69728f21262dfa
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-04-26 11:35:44 +0300

    Fix refreshing page issue

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ class Book {
 
   storeBook() {
     let book = {};
-    if (this.title && this.author !== '') {
+    if (this.title && this.author !== "") {
       book = {
         id: this.id,
         title: this.title,
@@ -19,56 +19,66 @@ class Book {
   }
 
   addBook() {
-    const bookItem = document.createElement('p');
-    bookItem.setAttribute('id', this.id);
-    const titlePara = document.createElement('p');
+    const bookItem = document.createElement("p");
+    bookItem.setAttribute("id", this.id);
+    const titlePara = document.createElement("p");
     titlePara.textContent = this.title;
-    const authorPara = document.createElement('p');
+    const authorPara = document.createElement("p");
     authorPara.textContent = this.author;
 
-    const lineBreak = document.createElement('hr');
+    const lineBreak = document.createElement("hr");
 
-    const books = document.querySelector('.books');
+    const books = document.querySelector(".books");
 
-    const deleteButton = document.createElement('button');
-    deleteButton.setAttribute('type', 'button');
-    deleteButton.setAttribute('class', 'delete');
-    deleteButton.textContent = 'Remove';
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("type", "button");
+    deleteButton.setAttribute("class", "delete");
+    deleteButton.textContent = "Remove";
 
     bookItem.appendChild(titlePara);
     bookItem.appendChild(authorPara);
     bookItem.appendChild(deleteButton);
     bookItem.appendChild(lineBreak);
 
-    deleteButton.addEventListener('click', () => this.removeBook(this.id));
+    deleteButton.addEventListener("click", () => {
+      this.removeBook(this.id);
+      books.removeChild(bookItem);
+    });
 
     return books.appendChild(bookItem);
   }
 
   removeBook(id) {
-    const newArray = JSON.parse(localStorage.getItem('booksList'));
-    newArray.filter(() => this.id !== id);
-
-    const booksId = document.getElementById(id);
-    return booksId.remove();
+    const newArray = JSON.parse(localStorage.getItem("booksList"));
+    const filteredArray = newArray.filter((book) => book.id !== id);
+    localStorage.setItem("booksList", JSON.stringify(filteredArray));
   }
 }
 
-const booksList = [];
+let booksList = [];
 
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+window.onload = () => {
+  booksList = JSON.parse(localStorage.getItem("booksList")) || [];
+  booksList.forEach((book) => {
+    const newBook = new Book(book.title, book.author);
+    newBook.id = book.id;
+    newBook.addBook();
+  });
+};
+
+const title = document.getElementById("title");
+const author = document.getElementById("author");
 
 // add the item to localstorage
-const form = document.querySelector('.add-book');
-form.addEventListener('submit', (e) => {
+const form = document.querySelector(".add-book");
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   const book = new Book(title.value, author.value);
   book.storeBook();
 
   book.addBook();
   booksList.push(book);
-  localStorage.setItem('booksList', JSON.stringify(booksList));
+  localStorage.setItem("booksList", JSON.stringify(booksList));
 
   form.reset();
 });
